5c80d110ef66515463bf639b036c3dba7716187a
atif <atif.ahmad80000@gmail.com>
2019-08-07 15:28:37 +0500

    Get method

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,6 +5,12 @@ const request=require('supertest');
 const {app}=require('./../server');
 const {toDo}=require('./../models/ToDo');
 
+// conts todos=[
+//   {text:"first todo"},
+//   {text:"second todo"},
+//   {text:"third todo"}]
+
+
 beforeEach((done)=>{
   toDo.remove({}).then(()=>done());
 });
@@ -29,6 +35,22 @@ describe('POST /todo',()=>{
       toDo.find().then((todos)=>{
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+
+  it('should not create',(done)=>{
+    request(app)
+    .post('/todo')
+    .send({})
+    .expect(400)
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      toDo.find().then((todos)=>{
+        expect(todos.length).toBe(0);
         done();
       }).catch((e)=>done(e));
     })
