17ac55b7d3de81cdf056d2c61ac8fc64330855bb
atif <atif.ahmad80000@gmail.com>
2019-08-08 09:30:32 +0500

    Add GET /todo/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+var {ObjectID}=require('mongodb');
 
 const {mongoose}=require('./db/mongoose');
 var {toDo}=require('./models/ToDo');
@@ -29,6 +29,29 @@ app.get('/todo',(req,res)=>{
     res.status(400).send(e);
   })
 })
+
+
+app.get('/todo/:id',(req,res)=>{
+  var id=req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  // res.status(200).send(id);
+
+  toDo.findById(id).then((succ)=>{
+    if (!succ) {
+      return res.status(404).send('not found');
+    }
+    res.status(200).send({succ})
+  },(err)=>{
+    res.status(400).send();
+  })
+
+
+
+})
+
 
 
 app.listen(3000,()=>{
